bc5d450267b3d2710d34493d0b3ef35785dc415d
audreywelch <welch.audreyl@gmail.com>
2019-06-07 09:23:37 -0700

    parsed commandline

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,36 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char commandline[2048];
+
+  // Get input from the keyboard
+  fgets(commandline, sizeof(commandline), stdin);
+
+  // Print user input to check
+  printf("User passed in: %s\n", commandline);
 
   // Open directory
 
-  // Repeatly read and print entries
+  // pointer for directory entry
+  struct dirent *de;
 
-  // Close directory
+  // Open the directory, returning a pointer of DIR type
+  // DIR *opendir(char *path);
+  
+  DIR *dr = opendir(commandline);
+  if (dr = NULL) {
+    printf("Could not open current directory");
+    return 0;
+  }
+  while ((de = readdir(dr)) != NULL) {
+    printf("%s\n", de->d_name);
+  }
+  
+
+  // // Repeatly read and print entries
+
+  // // Close directory
+  // closedir(dr);
 
   return 0;
 }
