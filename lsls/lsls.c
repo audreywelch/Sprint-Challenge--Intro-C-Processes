fcfff9265bc9f4f2f634e91abe766fa22a47401d
audreywelch <welch.audreyl@gmail.com>
2019-06-07 11:09:41 -0700

    Working on getting file size printed - only printing first file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -17,7 +18,62 @@ int main(int argc, char **argv)
     printf("   %s\n", argv[i]);
   }
 
+  // Print file path to check what's being returned
   printf("file to be opened: %s\n",argv[1]);
+
+  // Open directory
+
+  // Pointer for directory entry
+  struct dirent *de;
+
+  // Open the directory named in commandline, return a pointer of DIR type
+  DIR *dr = opendir(argv[1]);
+
+  // Check for errors
+  if (dr == NULL) {
+    printf("Could not open current directory\n");
+    exit(1);
+  }
+
+  struct stat buf;
+
+  // Repeatly read and print entries
+  while ((de = readdir(dr)) != NULL) {
+
+    
+
+    char *a = ("%s\n", argv[1]);
+    printf("a is %s\n", a);
+    char *b = ("/");
+    printf("b is %s\n", b);
+    // char *b = ("%s", de->d_name);
+    char *c = ("%s%s\n", a, b);
+    printf("%s\n", c);
+
+    char result[100] = "";
+    char result = snprintf(result, sizeof(result), "%s: %s: %s", argv[1], b, de->d_name);
+
+    printf("BLAH: %s\n", result);
+    char *full_path = ("%s: %s: %s\n", argv[1], b, de->d_name);
+    printf("full path for size: %s\n", full_path);
+
+    stat(full_path, &buf);
+    printf("file size is %lld\n", buf.st_size);
+
+    // print the name of the file within the directory entry
+    printf("%s\n", de->d_name);
+
+
+  }
+
+  // Close directory
+  closedir(dr);
+
+  return 0;
+}
+
+
+
 
   // // Create space for array storing commandline string
   // char commandline[2048];
@@ -27,33 +83,3 @@ int main(int argc, char **argv)
 
   // // Print user input to check
   // printf("User passed in: %s\n", commandline);
-
-  // Open directory
-
-  // Pointer for directory entry
-  struct dirent *de;
-
-  // Open the directory named in commandline, returning a pointer of DIR type
-  DIR *dr = opendir(argv[1]);
-
-  // Check for errors
-  if (dr == NULL) {
-    printf("Could not open current directory\n");
-    exit(1);
-  }
-
-  de = readdir(dr);
-  printf("%s\n", de->d_name);
-
-  // Repeatly read and print entries
-  while ((de = readdir(dr)) != NULL) {
-
-    // print the name of the file within the directory entry
-    printf("%s\n", de->d_name);
-  }
-
-  // Close directory
-  closedir(dr);
-
-  return 0;
-}
