00dca6e9c6a7de5077568a56d3ecb341419c015b
audreywelch <welch.audreyl@gmail.com>
2019-06-07 09:57:27 -0700

    Modified command line parsing, currently getting a NULL directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,36 +7,51 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  char commandline[2048];
 
-  // Get input from the keyboard
-  fgets(commandline, sizeof(commandline), stdin);
+  int i;
 
-  // Print user input to check
-  printf("User passed in: %s\n", commandline);
+  printf("There are %d command line argument(s):\n", argc);
+
+  // argv[0] is the file name
+  for (i = 0; i < argc; i++) {
+    printf("   %s\n", argv[i]);
+  }
+
+  // // Create space for array storing commandline string
+  // char commandline[2048];
+
+  // // Get input from the keyboard
+  // fgets(commandline, sizeof(commandline), stdin);
+
+  // // Print user input to check
+  // printf("User passed in: %s\n", commandline);
 
   // Open directory
 
-  // pointer for directory entry
+  // Pointer for directory entry
   struct dirent *de;
 
-  // Open the directory, returning a pointer of DIR type
-  // DIR *opendir(char *path);
-  
-  DIR *dr = opendir(commandline);
-  if (dr = NULL) {
-    printf("Could not open current directory");
-    return 0;
+  // Open the directory named in commandline, returning a pointer of DIR type
+  DIR *dr = opendir(argv[1]);
+
+  // Check for errors
+  if (dr == NULL) {
+    printf("Could not open current directory\n");
+    exit(1);
   }
+
+  de = readdir(dr);
+  printf("%s\n", de->d_name);
+
+  // Repeatly read and print entries
   while ((de = readdir(dr)) != NULL) {
+
+    // print the name of the file within the directory entry
     printf("%s\n", de->d_name);
   }
-  
 
-  // // Repeatly read and print entries
-
-  // // Close directory
-  // closedir(dr);
+  // Close directory
+  closedir(dr);
 
   return 0;
 }
